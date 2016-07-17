9293ee725c17f75922405dc3cce0b1349ca00486
Ray Guo <ihahoo@163.com>
2016-07-18 00:50:15 +0800

    Init

---
+++ webpack/webpack.config.server.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const autoprefixer = require('autoprefixer');
+const postcssNested = require('postcss-nested');
+const postcssMixins = require('postcss-mixins');
+const postcssSimpleVars = require('postcss-simple-vars');
+const CleanPlugin = require('clean-webpack-plugin');
+
+const rootPath = path.resolve(__dirname, '../');
+const srcPath = path.join(rootPath, '/src/');
+const distPath = path.join(rootPath, '/public/');
+
+const WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
+const webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack.isomorphic.tools'));
+
+const webpackConfig = {
+  devtool: false,
+  context: path.resolve(__dirname, '..'),
+  entry: {
+    main: ['babel-polyfill', srcPath + 'index']
+  },
+  output: {
+    path: distPath,
+    filename: 'js/[name].js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      { test: /\.(jsx|js)$/, include: srcPath, loaders: ['babel']},
+      { test: /\.json$/, include: srcPath, loader: 'json' },
+      { test: /\.css$/, include: srcPath, loader: ExtractTextPlugin.extract('style', 'css?modules&minimize&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss') },
+      {
+        test: webpackIsomorphicToolsPlugin.regular_expression('images'),
+        include: srcPath,
+        loader: 'url?limit=8192&name=images/[name].[ext]!image-webpack?{progressive:true, optimizationLevel: 7, svgo:{removeTitle:true,removeViewBox:false,removeRasterImages:true,sortAttrs:true,removeAttrs:false}}'
+      },
+      { test: /\.(woff2?|otf|eot|ttf)$/i, include: srcPath, loader: 'url?name=fonts/[name].[ext]' }
+    ],
+  },
+  postcss: function () {
+    return [postcssMixins, postcssSimpleVars, postcssNested, autoprefixer];
+  },
+  plugins: [
+    new CleanPlugin([distPath], {
+      root: rootPath
+    }),
+    new ExtractTextPlugin('css/[name].css'),
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        unused: true,
+        dead_code: true,
+        drop_debugger: true,
+        drop_console: true
+      }
+    }),
+    webpackIsomorphicToolsPlugin
+  ],
+  progress: true,
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  }
+}
+
+module.exports = webpackConfig;
