79def20c6177c94b79f87d795381854f2b3d1da7
Ray Guo <ihahoo@163.com>
2017-11-15 16:41:12 +0800

    fix eslint error

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -207,7 +207,7 @@ const webpackConfig = {
     }),
     new webpack.DefinePlugin({
       'process.env':{
-        'NODE_ENV': JSON.stringify('production')
+        NODE_ENV: JSON.stringify('production')
       }
     }),
     new UglifyJsPlugin()
--- webpack/webpack.config.server.js
+++ webpack/webpack.config.server.js
@@ -193,7 +193,7 @@ const webpackConfig = {
     }),
     new webpack.DefinePlugin({
       'process.env':{
-        'NODE_ENV': JSON.stringify('production')
+        NODE_ENV: JSON.stringify('production')
       }
     }),
     new UglifyJsPlugin(),
